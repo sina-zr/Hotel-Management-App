04d8590ef17d3d0aa71975dabb7a7310834d6745
Sina <sinazare1381@gmail.com>
2023-08-21 22:38:59 +0330

    Added Other Model in DataAccessLibrary

---
+++ DataAccessLibrary/Models/BookingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccessLibrary.Models
+{
+    public class BookingModel
+    {
+        public int Id { get; set; }
+        public GuestModel Geust { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool CheckedIn { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
