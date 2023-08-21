7951d155b02e7268e559a4c116b8e2f6a100df6c
Sina <sinazare1381@gmail.com>
2023-08-21 21:53:20 +0330

    Created DataAccessLibrary and SQL\Dapper DataAccess.cs

---
+++ DataAccessLibrary/SQL/Dapper/SQLDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.SQL.Dapper
+{
+    internal class SQLDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
