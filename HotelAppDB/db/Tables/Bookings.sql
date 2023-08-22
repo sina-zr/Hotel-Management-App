b82edf3bea41b5b4677ecc2e282bb8d15c27d588
Sina <sinazare1381@gmail.com>
2023-08-22 11:00:33 +0330

    Changed start & endDate int bookings table in SQL DataBase from  datetime2 into date AND published it

--- HotelAppDB/db/Tables/Bookings.sql
+++ HotelAppDB/db/Tables/Bookings.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[Bookings]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [RoomId] INT NOT NULL, 
-    [GuestId] INT NOT NULL, 
-    [StartDate] DATETIME2 NOT NULL, 
-    [EndDate] DATETIME2 NOT NULL, 
-    [checkedIn] BIT NOT NULL DEFAULT 0, 
-    [TotalCost] MONEY NOT NULL, 
-    CONSTRAINT [FK_Bookings_Rooms] FOREIGN KEY (RoomId) REFERENCES Rooms(Id), 
-    CONSTRAINT [FK_Bookings_Guests] FOREIGN KEY (GuestId) REFERENCES Guests(Id)
+	[RoomId] INT NOT NULL, 
+	[GuestId] INT NOT NULL, 
+	[StartDate] DATE NOT NULL, 
+	[EndDate] DATE NOT NULL, 
+	[checkedIn] BIT NOT NULL DEFAULT 0, 
+	[TotalCost] MONEY NOT NULL, 
+	CONSTRAINT [FK_Bookings_Rooms] FOREIGN KEY (RoomId) REFERENCES Rooms(Id), 
+	CONSTRAINT [FK_Bookings_Guests] FOREIGN KEY (GuestId) REFERENCES Guests(Id)
 )
