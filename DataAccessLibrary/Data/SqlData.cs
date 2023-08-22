ca8b4e7683b8d0a7945eabbfb422e2ab2cb8e815
Sina <sinazare1381@gmail.com>
2023-08-22 21:58:58 +0330

    Added BookARoom method to DataAccessLibrary.Data.SqlData.cs & created some Stored Procedures for it as well.

--- DataAccessLibrary/Data/SqlData.cs
+++ DataAccessLibrary/Data/SqlData.cs
@@ -28,5 +28,36 @@ namespace DataAccessLibrary.Data
                                                         connectionStringName,
                                                         true);
         }
+
+        public void BookARoom(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+        {
+            // if guest doesn't already exist in Guest Table, INSERT it
+            // & anyway return an Id
+            var guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuest_Insert",
+                                                          new { firstName, lastName },
+                                                          connectionStringName,
+                                                          true).First();
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+                new { Id = roomTypeId }, connectionStringName, false).First();
+
+            TimeSpan timeStaying = endDate.Subtract(startDate);
+            
+
+            // Getting the available rooms
+            var LoadRoomsParameters = new { startDate, endDate, roomTypeId };
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                              LoadRoomsParameters,
+                                                                              connectionStringName,
+                                                                              true);
+
+            //Saving the Booking
+            var SaveBookingParameters = new { roomId = availableRooms.First().Id,
+                startDate = startDate,
+                endDate = endDate,
+                totalCost = timeStaying.Days * roomType.Price };
+
+            _db.SaveData("dbo.spBookings_Insert", SaveBookingParameters, connectionStringName, true);
+        }
     }
 }
--- DataAccessLibrary/Models/BookingModel.cs
+++ DataAccessLibrary/Models/BookingModel.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; }
         public GuestModel Geust { get; set; }
+        public RoomTypeModel RoomType { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public bool CheckedIn { get; set; }
-        public decimal TotalPrice { get; set; }
+        public bool? CheckedIn { get; set; }
+        public decimal? TotalPrice { get; set; }
     }
 }
