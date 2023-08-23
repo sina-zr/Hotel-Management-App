8965860fcf49cd2ff8e1a20658db95eb75e546bd
Sina <sinazare1381@gmail.com>
2023-08-23 08:42:25 +0330

    Added Ignored Stored Procedures by Git which where already created before.

---
+++ HotelAppDB/db/Stored Procedures/spBookings_Insert.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Insert]
+	@roomId int,
+	@guestID int,
+	@startDate date,
+	@endDate date,
+	@totalCost money
+AS
+
+BEGIN
+	set nocount on;
+
+	insert into dbo.Bookings (RoomId, GuestId, EndDate, TotalCost)
+	values (@roomId, @guestID, @startDate, @endDate, @totalCost);
+
+END
