950b135724003a4553811c05618e4c51c23a77ab
Sina <sinazare1381@gmail.com>
2023-08-30 11:13:19 +0330

    Created HotelApp.Desktop WPF project & Created appsettings.json file & Configured Dependency Injection and ConnectionString in OnStartup overrided method

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLibrary.Data;
+using DataAccessLibrary.DataBase;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Overriding the OnStartup method from Application.cs
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Adding MainWindow to our Dependency Injection
+            // Adding some DataAccessLibrary instances
+            // gives you an instance each time
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddSingleton<IDataBaseData, SqlData>();
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+
+            // putting config into Dependency Injection
+            services.AddSingleton(config);
+
+            // Talking to our serviceProvider
+            var serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+}
