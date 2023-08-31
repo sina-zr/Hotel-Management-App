9915942f003063dcb5f15dcedad521e069536c4e
Sina <sinazare1381@gmail.com>
2023-08-31 09:51:29 +0330

    Added methods BookARoom and SearchBookings to SQLiteData.cs

--- DataAccessLibrary/Data/SQLiteData.cs
+++ DataAccessLibrary/Data/SQLiteData.cs
@@ -45,42 +45,84 @@ namespace DataAccessLibrary.Data
 
 		public void BookARoom(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
 		{
+			firstName.ToLower();
+			lastName.ToLower();
+
 			// if guest doesn't already exist in Guest Table, INSERT it
 			// & anyway return an Id
-			var guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuest_Insert",
+			// we have to do its in multiple calls as well cause SQLite doesn't have if begin end.
+			string query = @"select 1 from Guests where FirstName = @firstName and LastName = @lastName";
+			int results = _db.LoadData<dynamic, dynamic>(query, new { firstName, lastName }, connectionStringName).Count();
+
+			if (results == 0)
+			{
+				query = @"insert into Guests (FirstName, LastName) values (@firstName, @lastName);";
+				_db.SaveData(query, new { firstName, lastName }, connectionStringName);
+			}
+
+			query = @"select [Id], [FirstName], [LastName]
+					from Guests
+					where FirstName = @firstName and LastName = @lastName LIMIT 1;";
+
+			GuestModel guest = _db.LoadData<GuestModel, dynamic>(query,
 														  new { firstName, lastName },
 														  connectionStringName).First();
 
-			RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+			RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from RoomTypes where Id = @Id",
 				new { Id = roomTypeId }, connectionStringName).First();
 
 			TimeSpan timeStaying = endDate.Subtract(startDate);
 
 
 			// Getting the available rooms
+			query = @"select r.*
+			from Rooms r
+			inner join RoomTypes t on t.Id = r.RoomTypeId
+			WHERE r.RoomTypeId = @roomTypeId
+				AND r.Id NOT in (
+				SELECT b.RoomId
+				FROM Bookings b
+				WHERE (@startDate < b.StartDate AND @endDate > b.EndDate)
+					 OR (b.StartDate < @endDate AND @endDate < b.EndDate)
+					 OR (b.StartDate <= @startDate AND @startDate < b.EndDate));";
 			var LoadRoomsParameters = new { startDate, endDate, roomTypeId };
-			List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+			List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>(query,
 																			  LoadRoomsParameters,
 																			  connectionStringName);
 
 			//Saving the Booking
+			query = @"insert into Bookings (RoomId, GuestId, StartDate, EndDate, TotalCost)
+			values (@roomId, @guestId, @startDate, @endDate, @totalCost);";
 			var SaveBookingParameters = new
 			{
 				roomId = availableRooms.First().Id,
-				GuestId = guest.Id,
-				startDate = startDate,
-				endDate = endDate,
+				guestId = guest.Id,
+				startDate = startDate.ToString("yyyy-MM-dd"),
+				endDate = endDate.ToString("yyyy-MM-dd"),
 				totalCost = timeStaying.Days * roomType.Price
 			};
 
-			_db.SaveData("dbo.spBookings_Insert", SaveBookingParameters, connectionStringName);
+			_db.SaveData(query, SaveBookingParameters, connectionStringName);
 		}
 
 		public List<FullBookingModel> SerachBookings(string lastName)
 		{
+			lastName.ToLower();
 
-			var bookings = _db.LoadData<FullBookingModel, dynamic>("dbo.spBookings_Search",
-																   new { lastName, startDate = DateTime.Now.Date },
+			string query = @"
+			select [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate], [b].[checkedIn], [b].[TotalCost],
+				g.FirstName, g.LastName,
+				r.RoomNumber, r.RoomTypeId,
+				[rt].[Title], [rt].[Description]
+			from Bookings b
+			inner join Guests g on g.Id = b.GuestId
+			inner join Rooms r on r.Id = b.RoomId
+			inner join RoomTypes rt on rt.Id = r.RoomTypeId
+			where b.StartDate = @startDate AND g.LastName = @lastName;";
+
+
+			var bookings = _db.LoadData<FullBookingModel, dynamic>(query,
+																   new { lastName , startDate = DateTime.Now.Date.ToString("yyyy-MM-dd") },
 																   connectionStringName);
 
 			return bookings;
@@ -94,7 +136,7 @@ namespace DataAccessLibrary.Data
 
 		public RoomTypeModel GetRoomTypeById(int id)
 		{
-			return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetById",
+			return _db.LoadData<RoomTypeModel, dynamic>("Select * from RoomTypes where Id = @id;",
 				 new { id },
 				 connectionStringName).First();
 		}
