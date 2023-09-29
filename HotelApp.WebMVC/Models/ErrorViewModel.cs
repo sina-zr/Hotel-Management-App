ef4c0e9b1ea1eb9600ae7ac8f5ef6665595de308
Sina <sinazare1381@gmail.com>
2023-09-29 11:57:30 +0330

    Created MVC project

---
+++ HotelApp.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelApp.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
